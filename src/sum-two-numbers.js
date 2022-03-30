e7ddb8300acf162712b276a421b2b493e2e5958a
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-03-30 14:22:39 +0100

    Add feature sum two numbers
    const num1 & num2, return

---
+++ src/sum-two-numbers.js
@@ -0,0 +1,9 @@
+// to store input numbers
+const num1 = parseInt(prompt('Enter the first number:'));
+const num2 = parseInt(prompt('Enter the second number:'));
+
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+sum();
