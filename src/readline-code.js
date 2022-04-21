554e1f9ca8c79f8edcda617ccca92dc6bd9cbd3f
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-04-21 23:37:57 +0100

    Readline module code
    Option saved to run the program in the terminal

---
+++ src/readline-code.js
@@ -0,0 +1,66 @@
+import rl from 'readline';
+
+var readline = rl.createInterface(
+    process.stdin,
+    process.stdout
+);
+
+readline.question('\nChoose the operation:\n  [0] Sum\n  [1] Subtract\n  [2] Multiply\n  [3] Divide\n> ', function (inputedOperation) {
+
+    if (inputedOperation == '0') {
+        readline.question('Enter first number: ', (num) => {
+            const firstNumber = parseFloat(num);
+
+            readline.question('Enter second number: ', (num) => {
+                const secondNumber = parseFloat(num);
+
+                const result = firstNumber + secondNumber
+                console.log(`The sum of ${firstNumber} with ${secondNumber} is: ${result}`);
+
+                readline.close();
+            });
+        });
+    }
+    else if (inputedOperation == '1') {
+        readline.question('Enter first number: ', (num) => {
+            const firstNumber = parseFloat(num);
+
+            readline.question('Enter second number: ', (num) => {
+                const secondNumber = parseFloat(num);
+
+                const result = firstNumber - secondNumber
+                console.log(`The subtraction of ${firstNumber} with ${secondNumber} is: ${result}`);
+
+                readline.close();
+            });
+        });
+    }
+    else if (inputedOperation == '2') {
+        readline.question('Enter first number: ', (num) => {
+            const firstNumber = parseFloat(num);
+
+            readline.question('Enter second number: ', (num) => {
+                const secondNumber = parseFloat(num);
+
+                const result = firstNumber * secondNumber
+                console.log(`The multiplication of ${firstNumber} with ${secondNumber} is: ${result}`);
+
+                readline.close();
+            });
+        });
+    }
+    else if (inputedOperation == '3') {
+        readline.question('Enter first number: ', (num) => {
+            const firstNumber = parseFloat(num);
+
+            readline.question('Enter second number: ', (num) => {
+                const secondNumber = parseFloat(num);
+
+                const result = firstNumber / secondNumber
+                console.log(`The division of ${firstNumber} with ${secondNumber} is: ${result}`);
+
+                readline.close();
+            });
+        });
+    }
+});
