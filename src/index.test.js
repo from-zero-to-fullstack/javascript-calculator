5a37c1d72da5eb5453ea96f5c8387b1d34885ce7
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-04-28 23:43:18 +0100

    Improved Tests Sum function
    added more unitary tests

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,46 @@
+const { test, expect } = require('@jest/globals');
 const { sum, subtract, divide, multiply } = require('./index');
 
 test('I expect that 2 + 2 equals 4', () => {
-    expect(sum(2,2)).toBe(4);
+    expect(sum(2, 2)).toBe(4);
 })
 
-test('I expect that 10 + 10 equals to 20', () => {
-    expect(sum(10, 10)).toBe(20);
+test('I expect that 0 + 0 equals 0', () => {
+    expect(sum(0, 0)).toBe(0);
 })
 
-test('I expect that 10 + 50 equals to 60', () => {
-    expect(sum(10, 50)).toBe(60);
+test('I expect that -1 + 2 equals to 1', () => {
+    expect(sum(-1, 2)).toBe(1);
 })
 
-test('I expect that 100 + 100 equals to 200', () => {
-    expect(sum(100, 100)).toBe(200);
+test('I expect that 5 + -3 equals to 2', () => {
+    expect(sum(5, -3)).toBe(2);
 })
 
+test('I expect that -1 + -2 equals to -3', () => {
+    expect(sum(-1, -2)).toBe(-3);
+})
+
+test('I expect that 1.25 + 0.75 equals to 2', () => {
+    expect(sum(1.25, 0.75)).toBe(2);
+})
+
+test('I expect that 6 + 0.75 equals to 6.75', () => {
+    expect(sum(6, 0.75)).toBe(6.75);
+})
+
+test('I expect that 1.50 + 4 equals to 5.50', () => {
+    expect(sum(1.50, 4)).toBe(5.50);
+})
+
+test('I expect that 5 + null equals to 5', () => {
+    expect(sum(5, null)).toBe(5);
+})
+
+test('I expect that null + 5 equals to 5', () => {
+    expect(sum(null, 5)).toBe(5);
+})
+
+test('I expect that null + null equals to 0', () => {
+    expect(sum(null, null)).toBe(0);
+})
