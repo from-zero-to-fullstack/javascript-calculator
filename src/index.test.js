70e4107a54f12923183b9898ee103726d4a87af6
ma-oliveiramarques <ma_oliveiramarques@hotmail.com>
2022-04-29 00:47:01 +0100

    Added unitary tests to subtract function

--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,7 @@
 const { test, expect } = require('@jest/globals');
 const { sum, subtract, divide, multiply } = require('./index');
 
+//Sum function unitary tests
 test('I expect that 2 + 2 equals 4', () => {
     expect(sum(2, 2)).toBe(4);
 })
@@ -43,4 +44,57 @@ test('I expect that null + 5 equals to 5', () => {
 
 test('I expect that null + null equals to 0', () => {
     expect(sum(null, null)).toBe(0);
+})
+
+// Subtract function unitary tests
+test('I expect that 0 - 0 equals to 0', () => {
+    expect(subtract(0 , 0)).toBe(0);
+})
+
+test('I expect that 5 - 0 equals to 5', () => {
+    expect(subtract(5 , 0)).toBe(5);
+})
+
+test('I expect that 0 - 5 equals to -5', () => {
+    expect(subtract(0 , 5)).toBe(-5);
+})
+
+test('I expect that -5 - -5 equals to 0', () => {
+    expect(subtract(-5 , -5)).toBe(0);
+})
+
+test('I expect that -5 - -10 equals to 5', () => {
+    expect(subtract(-5 , -10)).toBe(5);
+})
+
+test('I expect that -10 - -1 equals to -9', () => {
+    expect(subtract(-10 , -1)).toBe(-9);
+})
+
+test('I expect that 10 - null equals to 10', () => {
+    expect(subtract(10 , null)).toBe(10);
+})
+
+test('I expect that null - 1 equals to -1', () => {
+    expect(subtract(null, 1)).toBe(-1);
+})
+
+test('I expect that null - null equals to 0', () => {
+    expect(subtract(null , null)).toBe(0);
+})
+
+test('I expect that 1 - 0.25 equals to 0.75', () => {
+    expect(subtract(1 , 0.25)).toBe(0.75);
+})
+
+test('I expect that 4.25 - 1 equals to 3.25', () => {
+    expect(subtract(4.25 , 1)).toBe(3.25);
+})
+
+test('I expect that 3.25 - 0.25 equals to 3', () => {
+    expect(subtract(3.25 , 0.25)).toBe(3);
+})
+
+test('I expect that 0.25 - 3.25 equals to -3', () => {
+    expect(subtract(0.25 , 3.25)).toBe(-3);
 })
